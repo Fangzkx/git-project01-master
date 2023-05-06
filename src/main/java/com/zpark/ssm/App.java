237ef39a843f4112501061d574a256b950704833
zkx <zkx@136.com>
2023-05-06 18:26:18 +0800

    zkx first commit

---
+++ src/main/java/com/zpark/ssm/App.java
@@ -0,0 +1,14 @@
+package com.zpark.ssm;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello git zkx second commit");
+        System.out.println("lzlzlzlz");
+        System.out.println("kakkak");
+        System.out.println("zczczczc");
+        System.out.println("8888");
+        System.out.println("100");
+        System.out.println("100");
+    }
+}
